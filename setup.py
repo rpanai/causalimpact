797438dd226f5120b1f890849c8e4bf85dd30edc
rpanai <robertopanai@sardus.it>
2020-02-03 12:59:07 -0300

    statsmodels>=0.9.0 only

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ config = {
     'author': 'Jamal Senouci',
     'url': 'http://jamalsenouci.github.io/causalimpact.html',
     'download_url': 'https://ppi.python.org/pypi/causalimpact/',
-    'version': '0.2.0',
+    'version': '0.2.1',
     'platforms': ['Linux', 'Mac OSX', 'Windows', 'Unix'],
     'install_requires': ['nose'],
     'packages': ['CausalImpact'],
